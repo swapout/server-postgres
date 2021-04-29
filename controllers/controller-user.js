4e2ac5b19597b7d82c3b7b6504b2fe7bf6cca097
Gabor Csecsetka <csecsi85@gmail.com>
2021-04-29 20:51:32 +0100

    Added error handling on mailgun send function

--- controllers/controller-user.js
+++ controllers/controller-user.js
@@ -723,8 +723,14 @@ exports.passwordReset = async (req, res) => {
 
         // Send email to user
         await mailgun.messages().send(data, (error, body) => {
-          console.log(body);
-        });
+          if(error) {
+            console.log(error)
+            return res.status(500).json({
+              status: 500,
+              message: error.message
+            })
+          }
+        })
       }
     }
 
