0562bfc7acb94c4bfc0935103a112569ce472e73
Gabor Csecsetka <csecsi85@gmail.com>
2021-07-30 01:14:03 +0100

    Added morgen logging middleware

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const config = require("config");
 const cookieParser = require("cookie-parser");
+const morgan = require("morgan");
+const path = require("path");
+const rfs = require("rotating-file-stream");
 
 const indexRoute = require("./routes/router-index");
 
@@ -15,6 +18,18 @@ app.use(cors({ credentials: true, origin: config.get("client.url") }));
 app.use(cookieParser(config.get("cookie.secret")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// create a rotating write stream
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "7d", // rotate daily
+  path: path.join(__dirname, "log"),
+});
+
+// setup the logger
+app.use(
+  morgan(":date[iso] -- :status -- :method -- :url -- :response-time ms", {
+    stream: accessLogStream,
+  })
+);
 
 app.use("/", indexRoute);
 
